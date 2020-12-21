e4cebf96de140a5c54f7d8f928132963480b0d20
李明飞 <mingfei.li@supers.io>
2020-12-21 19:36:09 +0800

    1

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,6 @@
-import Head from 'next/head'
-import Layout, { siteTitle } from '../components/layout'
-import utilStyles from '../styles/utils.module.css'
+import Head from "next/head";
+import Layout, { siteTitle } from "../components/layout";
+import utilStyles from "../styles/utils.module.css";
 
 export default function Home() {
   return (
@@ -11,10 +11,10 @@ export default function Home() {
       <section className={utilStyles.headingMd}>
         <p>I Am Li Ming Fei</p>
         <p>
-          (This is a sample website - you’ll be building a site like this on{' '}
+          (This is a sample website - you’ll be building a site like this on{" "}
           <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
         </p>
       </section>
     </Layout>
-  )
+  );
 }
